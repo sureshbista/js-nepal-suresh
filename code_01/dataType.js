e28100d87f3724b31bb2932a93703c635a9e0f92
Suresh Bista <sureshbista489@gmail.com>
2024-11-16 11:01:26 +0000

    Datatypes

---
+++ code_01/dataType.js
@@ -0,0 +1,25 @@
+//"user strict"; // treat all js code as newr as newer version
+
+//alert('hello')//we are using nodejs, not browser
+
+console.log(6+6)
+
+console.log("Suersh bist")
+
+let name ="suresh"
+let age = 28
+let isLoggeddIn =false
+let state;
+
+//number =>2 to power 23
+//bigint
+//string =>""
+//boolean =>ture /false
+//null =>standlone value
+//undefined =>
+//symbol =>unique
+
+
+//object =>
+console.log(typeof undefined );//undefine
+console.log(null)//object

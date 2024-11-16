698ef6acb33411e6a118609c493fdb1de359cf7d
Suresh Bista <sureshbista489@gmail.com>
2024-11-16 11:20:10 +0000

    Conversion of function

---
+++ code_01/function.js
@@ -0,0 +1,29 @@
+let score = "suresh"
+
+//console.log(typeof score)
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" =>33
+//"3abc"=NaN
+//ture =>1; flase =>0
+
+
+let isLoggeddIn = "suesh bist"
+let booleanIsLoggedIn = Boolean(isLoggeddIn)
+//console.log(booleanIsLoggedIn);
+
+//1 =>ture; 0 => false
+// "" =>false
+//"suersh" =>true
+
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

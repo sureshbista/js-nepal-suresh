cae3192dc183198b556bc34a7b0054ecefe9d966
Suresh Bista <sureshbista489@gmail.com>
2024-11-17 17:09:00 +0000

    string.js

---
+++ code_01/string.js
@@ -0,0 +1,205 @@
+const name ="suresh"
+const repoCount = 50
+
+// console.log(name +   repoCount);
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String ('suersh-gc-com')
+
+// console.log(gameName [0]);
+// console.log(gameName._proto_);
+
+
+console.log (gameName.length);
+console.log (gameName.toUpperCase());
+console.log (gameName.charAt(4));
+console.log (gameName.indexOf('h'));
+
+const newSting  = gameName.substring (0,6)
+console.log(newSting);
+
+
+const anotherString = gameName.slice(6,0)
+console.log(anotherString);
+
+
+const newStingOne = "  sruesh "
+console.log(anotherString)
+console.log (anotherString)
+
+
+const url = "https://suresh.com/suresh%20bista"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('suresh'))
+console.log(url.includes('ram'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+// anchor
+// : 
+// ƒ anchor()
+// at
+// : 
+// ƒ at()
+// big
+// : 
+// ƒ big()
+// blink
+// : 
+// ƒ blink()
+// bold
+// : 
+// ƒ bold()
+// charAt
+// : 
+// ƒ charAt()
+// charCodeAt
+// : 
+// ƒ charCodeAt()
+// codePointAt
+// : 
+// ƒ codePointAt()
+// concat
+// : 
+// ƒ concat()
+// constructor
+// : 
+// ƒ String()
+// endsWith
+// : 
+// ƒ endsWith()
+// fixed
+// : 
+// ƒ fixed()
+// fontcolor
+// : 
+// ƒ fontcolor()
+// fontsize
+// : 
+// ƒ fontsize()
+// includes
+// : 
+// ƒ includes()
+// indexOf
+// : 
+// ƒ indexOf()
+// isWellFormed
+// : 
+// ƒ isWellFormed()
+// italics
+// : 
+// ƒ italics()
+// lastIndexOf
+// : 
+// ƒ lastIndexOf()
+// length
+// : 
+// 0
+// link
+// : 
+// ƒ link()
+// localeCompare
+// : 
+// ƒ localeCompare()
+// match
+// : 
+// ƒ match()
+// matchAll
+// : 
+// ƒ matchAll()
+// normalize
+// : 
+// ƒ normalize()
+// padEnd
+// : 
+// ƒ padEnd()
+// padStart
+// : 
+// ƒ padStart()
+// repeat
+// : 
+// ƒ repeat()
+// replace
+// : 
+// ƒ replace()
+// replaceAll
+// : 
+// ƒ replaceAll()
+// search
+// : 
+// ƒ search()
+// slice
+// : 
+// ƒ slice()
+// small
+// : 
+// ƒ small()
+// split
+// : 
+// ƒ split()
+// startsWith
+// : 
+// ƒ startsWith()
+// strike
+// : 
+// ƒ strike()
+// sub
+// : 
+// ƒ sub()
+// substr
+// : 
+// ƒ substr()
+// substring
+// : 
+// ƒ substring()
+// sup
+// : 
+// ƒ sup()
+// toLocaleLowerCase
+// : 
+// ƒ toLocaleLowerCase()
+// toLocaleUpperCase
+// : 
+// ƒ toLocaleUpperCase()
+// toLowerCase
+// : 
+// ƒ toLowerCase()
+// toString
+// : 
+// ƒ toString()
+// toUpperCase
+// : 
+// ƒ toUpperCase()
+// toWellFormed
+// : 
+// ƒ toWellFormed()
+// trim
+// : 
+// ƒ trim()
+// trimEnd
+// : 
+// ƒ trimEnd()
+// trimLeft
+// : 
+// ƒ trimStart()
+// trimRight
+// : 
+// ƒ trimEnd()
+// trimStart
+// : 
+// ƒ trimStart()
+// valueOf
+// : 
+// ƒ valueOf()
+// Symbol(Symbol.iterator)

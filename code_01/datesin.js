e71d93d5c6e8a1dc8984c0fbc108912fcc36b5ed
Suresh Bista <sureshbista489@gmail.com>
2024-11-17 18:44:46 +0000

    datesin in js

---
+++ code_01/datesin.js
@@ -0,0 +1,41 @@
+//Date
+
+let myDate = new Date()
+console.log(myDate.toString());
+
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+
+// let myCreateDate =new Date(2024, 0, 23)
+// let myCreateDate =new Date(2024, 0, 23)
+// let myCreateDate =new Date(2024, 0, 4)
+
+let myCreateDate = new  Date("01-14-2024")
+
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate.toISOString());
+// console.log(myCreateDate.toJSON());
+console.log(myCreateDate.toLocaleString());
+
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate  = new Date()
+console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+// console.log(newDate.getUTCSeconds());
+// console.log(newDate.getMilliseconds());
+

1e58f5ef63b76a88053f74eb39fc8b298a78e761
Suresh Bista <sureshbista489@gmail.com>
2024-11-17 12:35:36 +0000

    datatypes_summary

---
+++ code_01/datatypes_summary.js
@@ -0,0 +1,35 @@
+//primitive
+
+//7 types: string, Numbers, Boolearn, null,  underfined, symbol, BigInt
+
+const score =100
+const scoreValue =100.3
+
+const isLoggeddIn = false
+const outsideTemp = null
+let useEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log (id === anotherId);
+
+//const bigNumber = 12354557n
+
+
+
+//Reference (non  primitives)
+//Arry, Object, Functions
+
+const heros =["suresh", "Hari", "manoj", "gopal"];
+
+let myObj = {
+    name : "suersh",
+    age  : 19,
+
+};
+const myFuncation = function(){
+    console.log("helow world !");
+}
+
+console.log(typeof outsideTemp);

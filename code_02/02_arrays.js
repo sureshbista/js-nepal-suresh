e3c5a8c091f179f26c0c2c85432f43cc24a013a2
Suresh Bista <sureshbista489@gmail.com>
2024-11-23 09:13:26 +0000

    Array Concept 2

---
+++ code_02/02_arrays.js
@@ -0,0 +1,26 @@
+// const marvel_heros =["ram", "suresh", "Naresh"]
+// const dc_heros =["superman", "flash", "batm"]
+
+// // marvel_heros.push(dc_heros)
+
+// // console.log(marvel_heros);
+// // console.log(marvel_heros[3][1]);
+
+
+
+
+// const anotehr_array = [1, 2, 3, [4, 5, 6],7,[6, 7,[4, 5]]]
+// const real_another_array= anotehr_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.isArray("suresh"))
+// console.log(Array.from("suresh"))
+
+// console.log(Array.from({name:"suresh"}))//interesting case
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1, score2, score3));
+

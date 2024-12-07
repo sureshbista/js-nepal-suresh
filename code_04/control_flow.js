3d127b1abfdefde5aac3a52ef0d72754f2d15cd1
Suresh Bista <sureshbista489@gmail.com>
2024-12-07 02:37:01 +0000

    controlFlow js

---
+++ code_04/control_flow.js
@@ -0,0 +1,58 @@
+// if
+// const isUserLoggedIn =true
+
+
+// const temperature =41
+// if (temperature === 41){
+//   console.log("Less than 50");
+// }
+// else{
+//     console.log("temperature is grater than 50");
+// }
+// console.log("Execute");
+
+
+// const score = 200
+
+// if(score >100){
+//     const power = "fly"
+//     console.log(`User power:${power}`);
+// }
+
+// console.log(`User power:${power}`);
+
+
+// const balance = 1000
+
+// if(balance >500) console.log("test"),
+// console.log("test2")
+
+// Qno 3 *************** coding javascript*********
+// const balance = 1000
+
+// if(balance < 500){
+// console.log("Less than");
+// }
+// else if(balance <750){
+//     console.log("Less than 750");
+// }
+// else if(balance < 900)
+// {
+//     console.log("Less than 900");
+// }
+// else{
+//     console.log("Less than 1200");
+// }
+
+
+const isUserLoggedIn = true
+const debitCard = true
+const loggeInFromGoogle = false
+const loggedInFromEmail = true
+
+if(isUserLoggedIn && debitCard && 2==3){
+    console.log("All to buy course");
+}
+if(loggeInFromGoogle || loggedInFromEmail){
+    console.log("User logged in ");
+}
